7d0d6236f1294ef3c8d4c7786aaee92f96c7f154
guizhihong <guizhihong@corp.netease.com>
2020-05-22 22:33:24 +0800

    [update][注释]添加注解解析任务的注释

--- check_plugin/src/main/java/com/ITaskFlowInstruction.java
+++ check_plugin/src/main/java/com/ITaskFlowInstruction.java
@@ -16,6 +16,7 @@ import java.util.List;
  * </>
  *
  * <p>
+ * {@link IAnnotationParser} 注解信息解析器，用来自动解析需要进行后续空指针分析的class文件
  * {@link IBytecodeParser} 字节码解析器接口，任务流的入口，借助Asm解析出判空需要的字节码信息
  * {@link IOpcodeFilter} 过滤器接口， 过滤出需要进行空指针检测分析的字节码指令集
  * {@link IOpcodeAnalyser} 空指针分析器接口，接受过滤器输出的数据进行空指针分析
