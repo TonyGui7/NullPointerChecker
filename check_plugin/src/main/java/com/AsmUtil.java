5c3e4f37bad7aa4fcaf968a938b048a1d3e30fae
guizhihong <guizhihong@corp.netease.com>
2021-02-23 16:16:43 +0800

    [add][output]添加分析结果输出

--- check_plugin/src/main/java/com/AsmUtil.java
+++ check_plugin/src/main/java/com/AsmUtil.java
@@ -1,6 +1,12 @@
 package com;
 
+import com.nullpointer.analysis.bean.AnalysisResultBean;
+
+import org.gradle.api.logging.Logging;
+
 import java.io.File;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class AsmUtil implements ITaskFlowInstruction.Callback {
     private ITaskFlowInstruction mManager;
@@ -31,5 +37,20 @@ public class AsmUtil implements ITaskFlowInstruction.Callback {
     @Override
     public void onTaskFinished(TaskBeanContract.ISimpleTaskOutput result) {
         mOutput = result;
+        if (result != null && result.getAnalysisResult() != null) {
+            List<AnalysisResultBean> analysisResultBeans = result.getAnalysisResult();
+            Logging.getLogger("NPChecker").error("\n/*****************************");
+            Logging.getLogger("NPChecker").error(" *       NPCheck results     *");
+            Logging.getLogger("NPChecker").error(" *****************************/\n");
+            for (AnalysisResultBean bean : analysisResultBeans) {
+                String classFullName = bean.clzzName.replace('/', '.');
+                String[] splits = Pattern.compile("[.]").split(classFullName);
+                String classSimpleName = (splits == null || splits.length <= 0) ? "" : splits[splits.length - 1];
+                String logInfo = classFullName + "." + bean.methodName + "(" + classSimpleName + ".java:"
+                        + bean.lineNumber + ")\n";
+                Logging.getLogger("NPChecker").warn(logInfo);
+            }
+            Logging.getLogger("NPChecker").error("/****************End*************/\n");
+        }
     }
 }
